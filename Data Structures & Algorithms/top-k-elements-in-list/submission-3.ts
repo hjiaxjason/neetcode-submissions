08bf8140762721c135af334e4e5afa10dbffd273
Jason Huang <105182872+hjiaxjason@users.noreply.github.com>
2026-07-22 14:14:56 -0700

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const freqMap = new Map<number, number>();
+        for (const num of nums) {
+            if (!freqMap.has(num)) {
+                freqMap.set(num, 1);
+                continue;
+            }
+            freqMap.set(num, freqMap.get(num)+1);
+        }
+
+        const groups: number[][] = Array.from({ length: nums.length + 1 }, () => []);
+
+        for (const [num, freq] of freqMap.entries()) {
+            groups[freq].push(num);
+        }
+
+        const result: number[] = [];
+        for (let i = nums.length; i >= 0; i--) {
+            for (const n of groups[i]) {
+                result.push(n);
+                if (result.length === k) {
+                    return result;
+                }
+            }
+        }
+        
+    }
+}
